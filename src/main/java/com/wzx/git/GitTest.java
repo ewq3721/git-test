fdf8b61a762c5396745bd1f7ea8e5eb1116b2cf0
wzx <ewq3721@163.com>
2021-08-12 12:14:04 +0800

    git  commit 02

--- src/main/java/com/wzx/git/GitTest.java
+++ src/main/java/com/wzx/git/GitTest.java
@@ -3,5 +3,6 @@ package com.wzx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println(" git test 01 ……");
+        System.out.println(" git test 02 ……");
     }
 }
